2ae9114a5f75d3516439365fd11f774c2e0f5195
Laure <laure.milian@gmail.com>
2017-01-17 15:43:54 +0100

    tp jusqu'à bonus validation'

---
+++ resources/views/twoot.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Un joli twoot</title>
+</head>
+<body>
+
+    <h1>{{$twoot->text}}</h1>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,37 @@
 
 use App\Twoot;
 
+
 Route::get('/', function () {
     // Récupérer tous les "twoots" et les ajouter à la vue
     return view('app')->with([
-        'twoots' => [] //Twoot::___()
+        'twoots' => Twoot::all()
     ]);
 });
 
 //Ici, décommenter et créer la route '/about'
-//Route::get('______', function(){
-//    return ____; //Renvoyer la vue 'about' !
-//});
+Route::get('about', function(){
+    return view('about'); //Renvoyer la vue 'about' !
+});
+
+Route::get('twoots/{id}', function($id) {
+	return view('twoot')->with([
+		'twoot' => Twoot::find($id)
+	]);
+});
+
 
 Route::post('twoots', function(){
-//    Twoot::______([
-//        'text' => request()->____
-//    ]);
-//
-//    return redirect()->to('_');
+    Twoot::create([
+        'text' => request()->text . ' bla bla'
+    ]);
+
+    return redirect()->to('/');
 });
 
 Route::delete('twoots/{id}', function($id){
 //    Trouver le twoot, puis le supprimer
-//    Twoot::____($id)->______();
+    Twoot::find($id)->delete();
 
     return redirect()->to('/');
 });
